16c8ca37f32bd15215209358e4683505b5665614
Jojobas <imalahov30@gmail.com>
2022-03-15 18:01:29 +0300

    added lib & tests

---
+++ toys_catalogue_lib/toys_catalogue.c
@@ -0,0 +1,5 @@
+//
+// Created by ivan on 15.03.2022.
+//
+
+#include "toys_catalogue.h"
---
+++ toys_catalogue_lib/toys_catalogue.h
@@ -0,0 +1,8 @@
+//
+// Created by ivan on 15.03.2022.
+//
+
+#ifndef C_HW_1_TOYS_CATALOGUE_H
+#define C_HW_1_TOYS_CATALOGUE_H
+
+#endif //C_HW_1_TOYS_CATALOGUE_H
